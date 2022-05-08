61f965c0bf141c70842bb2f1b31700623019a78e
Mihail <zxcvb787@yandex.ru>
2022-05-08 18:03:54 +0300

    Выполнил правки

--- src/com/company/lesson1/Homework1.java
+++ src/com/company/lesson1/Homework1.java
@@ -3,12 +3,9 @@ package com.company.lesson1;
 public class Homework1 {
     public static void main(String[] args) {
         task1();
-
-
     }
 
     public static void task1() {
-        System.out.println("task1 run");
-
+        System.out.println("task1 run!");
     }
 }
